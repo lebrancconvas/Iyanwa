f8b50ed0a781c711d6b1862af58a5a3d0a20d7f5
lebrancconvas <lebranccconvas@gmail.com>
2019-02-19 17:36:52 +0700

    Switch Case.

--- bot.js
+++ bot.js
@@ -6,8 +6,29 @@ iyanwa.on('ready', () => {
 })
 
 iyanwa.on('message', (message) => {
-    if (message.content === 'มาทิลด้า' || message.content.toLowerCase() === 'matilda') {
-        message.reply('อิหยังวะ!')
+    switch (message.content) {
+        case "มาทิลด้า":
+            message.reply("อิหยังวะ")
+            break;
+        case "Matilda":
+            message.reply("Iyanwa!!")
+            break;
+        case "matilda":
+            message.reply("iyanwa!!")
+            break;
+        case "ควย":
+            message.reply("พูดหยาบตบปากสามทีนะคะ")
+            break;
+        case "นะค่ะ":
+            message.reply("คุณพิมพ์ผิดค่ะ ที่ถูกต้องเป็น 'นะคะ' ไม่มีไม้เอกนะคะ ^^ ")
+            break;
+        case "ขอจีบหน่อย":
+            message.reply("ไม่ให้ค่ะ อิอิ แต่ถ้าอยากมาหาก็มาเชกิกับมาทิลด้าบ่อยๆน้าา ><~ ")
+            break;
+        case "มีตังค์ให้ยืมมั้ย":
+            message.reply("ไม่มีค่ะ อิอิ")
+            break;
+        default:
     }
 })
 
